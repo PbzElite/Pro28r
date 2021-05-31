bb6fc7f59f5e46113303f0aa444588b59fbfeeba
PbzElite <81586572+PbzElite@users.noreply.github.com>
2021-05-31 15:11:33 -0400

    Add files via upload

---
+++ Pro C28/js/launcher.js
@@ -0,0 +1,26 @@
+class Launcher{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.01,
+            length: 10
+        }
+        this.bodyA=bodyA;
+        this.pointB = pointB;
+        this.launcher = Constraint.create(options);
+        World.add(world,this.launcher);
+    }
+
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    attach(body){
+        this.launcher.bodyA=body;
+    }
+
+    display(){
+
+}
+}
